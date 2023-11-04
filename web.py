c0304baac8d91aa34fab3f2c691010583aa8a43b
Laynz Lost contact <131390720+laynz28@users.noreply.github.com>
2023-11-03 23:00:49 -0900

    Update web.py

--- web.py
+++ web.py
@@ -1,15 +1,19 @@
 import gradio as gr
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import openai
 
 openai.api_key = "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"
 
+# Load GPT-3 model using transformers library
+model_name = 'gpt-3'
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+
 def chatbot(input_text):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=input_text,
-        max_tokens=150
-    )
-    return response.choices[0].text
+    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+    response = model.generate(input_ids, max_length=150, num_return_sequences=1, no_repeat_ngram_size=2)
+    return tokenizer.decode(response[0], skip_special_tokens=True)
 
 iface = gr.Interface(
     fn=chatbot,
