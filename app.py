cac528c2e67a639965406b005220b422ba1a5ec4
Laynz Lost contact <131390720+laynz28@users.noreply.github.com>
2023-11-23 01:41:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, request
-
+import gradio as gr
 
 app = Flask(__name__)
 
+# Fungsi untuk mengganti (* *) dengan link karakter dari character.ai
+def replace_action(*https://c.ai/c/s8monVYz28ehmepyf1PbkP1bl7cj4e7-l5d5RNrdtnc*):
+    # Implementasi logika penggantian (* *) dengan link karakter
+    # Misalnya, Anda dapat menggunakan regex atau metode penggantian sederhana
+    # Di sini, saya contohkan dengan menggantikan (*nama_karakter*) dengan link ke character.ai/nama_karakter
+    replaced_text = text.replace("(*)", "[https://c.ai/c/s8monVYz28ehmepyf1PbkP1bl7cj4e7-l5d5RNrdtnc](https://character.ai)")
+    return replaced_text
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# Fungsi Gradio untuk memproses input dan mengembalikan output
+def chatbot(text):
+    processed_text = replace_action(text)
+    return processed_text
 
+# Gradio UI
+iface = gr.Interface(fn=chatbot, inputs="text", outputs="text")
+iface.launch(share=True)
 
-@app.route("/get")
-def get_bot_response():
-    query = request.args.get('msg')
-    response = "this is an answer"
-    return response
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=54321)
+if __name__ == '__main__':
+    app.run(debug=True)
