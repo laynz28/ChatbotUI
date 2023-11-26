b631b0c8b10247d7a35a60d7053f1df76d364303
Laynz Lost contact <131390720+laynz28@users.noreply.github.com>
2023-11-26 20:32:47 +0700

    Update app.py
    suck

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template, request
-import gradio as gr
+from flask import Flask, render_template, request, jsonify
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
-# Fungsi untuk mengganti (* *) dengan link karakter dari character.ai
-def replace_action(*https://c.ai/c/s8monVYz28ehmepyf1PbkP1bl7cj4e7-l5d5RNrdtnc*):
-    # Implementasi logika penggantian (* *) dengan link karakter
-    # Misalnya, Anda dapat menggunakan regex atau metode penggantian sederhana
-    # Di sini, saya contohkan dengan menggantikan (*nama_karakter*) dengan link ke character.ai/nama_karakter
-    replaced_text = text.replace("(*)", "[https://c.ai/c/s8monVYz28ehmepyf1PbkP1bl7cj4e7-l5d5RNrdtnc](https://character.ai)")
-    return replaced_text
+character = {
+    'name': 'Sky',
+    'icon': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIHsGvJIRzEGdp4oMicqCWppS3bZoxAsjvLQ&usqp=CAU',  # Ganti dengan URL ikon karakter
+    'description': 'an fnf fan girls.'
+}
 
-# Fungsi Gradio untuk memproses input dan mengembalikan output
-def chatbot(text):
-    processed_text = replace_action(text)
-    return processed_text
+@app.route('/')
+def index():
+    return render_template('index.html', character=character)
 
-# Gradio UI
-iface = gr.Interface(fn=chatbot, inputs="text", outputs="text")
-iface.launch(share=True)
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.form['user_message']
+    # Lakukan pemrosesan bahasa alami atau panggil model bahasa di sini
+    # Misalnya, kamu bisa menggunakan model GPT
+
+    # Contoh sederhana: balas dengan pesan yang sama dari pengguna
+    bot_response = f'Kamu berkata: {user_message}'
+
+    return jsonify({'bot_response': bot_response, 'character': character})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
